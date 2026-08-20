fb56c25c163951c4cc441735ee241255455ed56a
Angelo Theodorakis <silentmanww@gmail.com>
2026-08-20 21:24:48 +0100

    Updates logger fix

--- scripts/fedora_system_updates.sh
+++ scripts/fedora_system_updates.sh
@@ -16,28 +16,22 @@ log_entry() {
   local action="$1"
   local status="$2"
   local msg="$3"
-  logger -p user."${status}" -t "${action}" "fedora_secure_boot: ${msg}"
+  logger -p user."${status}" -t "${action}" "fedora_system_updates: ${msg}"
 }
 
-# Writing action output to cpe_logger
-current_time=$(/bin/date +"%Y-%m-%d %T %z")
-echo "$current_time; type: fedora_system_updates; action: started_dnf_updates; status: success; msg:  operating system updates started; " >> /var/log/cpe_logger.log
+log_entry "started_dnf_updates" "info" "operating system updates started" 
 
 # Run system updates
 echo -e "${Yellow}Running operating system updates...${Color_Off}\n"
-dnf -y upgrade --refresh --skip-unavailable --disable-repo=cpe-yum*
+dnf -y upgrade --refresh --skip-unavailable
 if [ $? -ne 0 ] && [ $? -ne 1 ]; then
     echo -e "${Red}ERROR${Color_Off}: dnf upgrade failed (exit code $?)."
-    # Writing action output to cpe_logger
-    current_time=$(/bin/date +"%Y-%m-%d %T %z")
-    echo "$current_time; type: fedora_system_updates; action: completed_dnf_updates; status: fail; msg:  operating system updates failed; " >> /var/log/cpe_logger.log
+    log_entry "completed_dnf_updates" "err" "operating system updates failed" 
     exit 1
 fi
 echo -e "${Green}operating system updates completed.${Color_Off}\n"
 
-# Writing action output to cpe_logger
-current_time=$(/bin/date +"%Y-%m-%d %T %z")
-echo "$current_time; type: fedora_system_updates; action: completed_dnf_updates; status: success; msg: operating system updates completed; " >> /var/log/cpe_logger.log
+log_entry "completed_dnf_updates" "info" "operating system updates completed" 
 
 # Update flatpaks
 if command -v flatpak >/dev/null 2>&1
@@ -48,9 +42,7 @@ then
         echo -e "${Yellow}Updating flatpak apps...${Color_Off}\n"
         flatpak update -y
         echo -e "${Green}flatpak apps updated.${Color_Off}\n"
-        # Writing action output to cpe_logger
-        current_time=$(/bin/date +"%Y-%m-%d %T %z")
-        echo "$current_time; type: fedora_system_updates; action: flatpak_updates; status: success; msg: flatpak apps updated; " >> /var/log/cpe_logger.log
+        log_entry "flatpak_updates" "info" "flatpak apps updated" 
         ;;
     n|N )
         echo -e "${Yellow}Skipping flatpak apps...${Color_Off}\n"
@@ -72,9 +64,7 @@ then
         echo -e "${Yellow}Updating device firmware...${Color_Off}\n"
         fwupdmgr update
         echo -e "${Green}Device firmware updated. Restart may be needed.${Color_Off}\n"
-        # Writing action output to cpe_logger
-        current_time=$(/bin/date +"%Y-%m-%d %T %z")
-        echo "$current_time; type: fedora_system_updates; action: firmware_updates; status: success; msg: Device firmware updated; " >> /var/log/cpe_logger.log
+        log_entry "firmware_updates" "info" "Device firmware updated" 
         ;;
     n|N )
         echo -e "${Yellow}Skipping firmware updates...${Color_Off}\n"
@@ -85,9 +75,7 @@ then
     esac
 fi
 
-# Writing action output to cpe_logger
-current_time=$(/bin/date +"%Y-%m-%d %T %z")
-echo "$current_time; type: fedora_system_updates; action: completed_fedora_system_updates; status: success; msg: fedora_system_updates completed; " >> /var/log/cpe_logger.log
+log_entry "completed_fedora_system_updates" "info" "fedora_system_updates completed" 
 
 read -r -p "Script completed. Would you like to reboot your device now? (y/n) " choice
 case "$choice" in
